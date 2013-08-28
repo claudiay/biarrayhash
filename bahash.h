15083d59f1cefe781dc09afdb06d8083d5856ff2
claudiay <yadathi@gmail.com>
2013-08-28 01:26:51 -0700

    added reverse lookup

--- bahash.h
+++ bahash.h
@@ -35,18 +35,53 @@ unsigned int hash(dict *htable, char *str) {
 
     for (hashval=0; *str != '\0'; str++) 
         hashval = *str + (hashval << 5) - hashval;
-    
+   
     return hashval % htable->size;
+}
+
+char *reverse_array(dict *htable, char *str, char *result, long buff_size) {
+    char *word, *tmp;
+    if ((tmp = malloc(buff_size)) == NULL) return NULL;
+
+    word = strtok(str, ",");
+    strcpy(result, word);
+
+    while (word != NULL) {
+        word = strtok(NULL, ",");
+        if (word != NULL) {
+            strcpy(tmp, word);
+            strcat(tmp, ",");
+            strcat(tmp, result);
+            strcpy(result, tmp);
+        }
+    }
+    free(tmp);
+    return result;
 }
 
 list *lookup_string(dict *htable, char *str) {
     list *d_list;
+    char *words, *reverse;
     unsigned int hashval = hash(htable, str);
+    long buff_size = sizeof(char *) * strlen(str) + 2;
 
     for (d_list=htable->table[hashval]; d_list != NULL; d_list=d_list->next)
         if (strcmp(str, d_list->string) == 0)
             return d_list;
+
+    words = strdup(str);
+
+    if ((reverse = malloc(buff_size)) == NULL) return NULL;
+    reverse = reverse_array(htable, words, reverse, buff_size);
+    for (d_list=htable->table[hashval]; d_list != NULL; d_list=d_list->next)
+        if (strcmp(reverse, d_list->string) == 0) {
+            free(reverse);
+            free(words);
+            return d_list;
+        }
     
+    free(reverse);
+    free(words);
     return NULL;
 }
 
